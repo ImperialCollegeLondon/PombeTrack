bde46cea3adee535c6d7af5aeac9143cdf33982d
mountainpenguin <pinguino.de.montana@googlemail.com>
2019-10-10 10:46:45 +0100

    Lint movie_generator.py
    Finish code-based linting
    
    Disable no-member warning for OutlineRow (pylint can't detect attributes
    properly).
    
    To-do:
        - docstrings
    
    pylint score: 6.49 -> 8.63 -> 9.24
    
    See #40

--- library/movie_generator.py
+++ library/movie_generator.py
@@ -17,59 +17,56 @@ class MovieMaker:
         self.image_loader = settings["image_loader"]
         self.screen_dpi = settings["screen_dpi"]
 
-        cells = self.get_cells(self.cell_id, settings["descendants"])
+        outlines = self.get_outlines(self.cell_id, settings["descendants"])
         # this bit groups outlines by frame index
-        self.cells = [
-            (frame_idx, list(cells))
-            for frame_idx, cells in
-            itertools.groupby(cells, lambda x: x.frame_idx)
+        self.outlines = [
+            (frame_idx, list(outlines))
+            for frame_idx, outlines in
+            itertools.groupby(outlines, lambda x: x.frame_idx)
         ]
 
         self.create_movie(
-            settings["outlines"],
+            settings["draw_outlines"],
             settings["frames"],
             settings["scale"],
             settings["channels"],
         )
 
-    def get_cells(self, cell_id, descendants):
-        cells = database.getOutlinesByCellId(cell_id)
+    def get_outlines(self, cell_id, descendants):
+        outlines = database.getOutlinesByCellId(cell_id)
 
         if not descendants:
-            return cells
+            return outlines
 
-        if cells[-1].child_id2:
-            child1 = database.getOutlineById(cells[-1].child_id1)
-            child2 = database.getOutlineById(cells[-1].child_id2)
-            cells.extend(self.get_cells(child1.cell_id, descendants))
-            cells.extend(self.get_cells(child2.cell_id, descendants))
+        if outlines[-1].child_id2:
+            child1 = database.getOutlineById(outlines[-1].child_id1)
+            child2 = database.getOutlineById(outlines[-1].child_id2)
+            # pylint warning disabled, OutlineRow does have a cell_id attribute
+            outlines.extend(self.get_outlines(child1.cell_id, descendants))  # pylint: disable=no-member
+            outlines.extend(self.get_outlines(child2.cell_id, descendants))  # pylint: disable=no-member
 
         return sorted(
-            cells,
+            outlines,
             key=lambda x: x.frame_idx
         )
 
-    def create_movie(self, outlines, frames, scale, channels):
-        out_path = os.path.join("data", "movies", self.experiment_id, self.cell_id)
-        if not os.path.exists(out_path):
-            os.makedirs(out_path)
-
-        c_temp = self.image_loader.load_frame(self.cells[0][0], 0)
+    def get_edges(self):
+        c_temp = self.image_loader.load_frame(self.outlines[0][0], 0)
         left_edge, right_edge = c_temp.shape[1], 0
         top_edge, bottom_edge = c_temp.shape[0], 0
-        for frame_idx, cells in self.cells:
-            for cell in cells:
-                if cell.offset_left < left_edge:
-                    left_edge = int(cell.offset_left)
+        for frame_data in self.outlines:
+            for outline in frame_data[1]:
+                if outline.offset_left < left_edge:
+                    left_edge = int(outline.offset_left)
 
-                if cell.offset_left + 150 > right_edge:
-                    right_edge = cell.offset_left + 150
+                if outline.offset_left + 150 > right_edge:
+                    right_edge = outline.offset_left + 150
 
-                if cell.offset_top < top_edge:
-                    top_edge = int(cell.offset_top)
+                if outline.offset_top < top_edge:
+                    top_edge = int(outline.offset_top)
 
-                if cell.offset_top + 150 > bottom_edge:
-                    bottom_edge = cell.offset_top + 150
+                if outline.offset_top + 150 > bottom_edge:
+                    bottom_edge = outline.offset_top + 150
 
                 delta = (right_edge - left_edge) - (bottom_edge - top_edge)
                 if delta > 0:
@@ -88,95 +85,117 @@ class MovieMaker:
                 if bottom_edge > c_temp.shape[0]:
                     bottom_edge = c_temp.shape[0]
 
-        for frame_idx, cells in self.cells:
-            channel1 = self.image_loader.load_frame(frame_idx, 0)
-            channel2, channel3 = None, None
+        return left_edge, right_edge, top_edge, bottom_edge
 
-            width_in = (bottom_edge - top_edge) / self.screen_dpi
-            if not channels or self.image_loader.num_channels == 1:
-                fig = matplotlib.figure.Figure(figsize=(width_in, width_in))
-                ax1 = fig.add_subplot(111)
-                ax2 = None
-                ax3 = None
-            elif channels and self.image_loader.num_channels == 2:
-                fig = matplotlib.figure.Figure(figsize=(width_in * 2, width_in))
-                ax1 = fig.add_subplot(121)
-                ax2 = fig.add_subplot(122)
-                channel2 = self.image_loader.load_frame(frame_idx, 1)
-                ax3 = None
-            elif channels and self.image_loader.num_channels == 3:
-                fig = matplotlib.figure.Figure(figsize=(width_in * 3, width_in))
-                ax1 = fig.add_subplot(131)
-                ax2 = fig.add_subplot(132)
-                ax3 = fig.add_subplot(133)
-                channel2 = self.image_loader.load_frame(frame_idx, 1)
-                channel3 = self.image_loader.load_frame(frame_idx, 2)
+    def create_axes(self, frame_idx, edges, channels):
+        channel1 = self.image_loader.load_frame(frame_idx, 0)
+        channel2, channel3 = None, None
 
-            for ax_idx, this_ax in enumerate([ax1, ax2, ax3]):
-                if not this_ax:
-                    break
-                this_ax.set_aspect("equal")
-                this_ax.axis("off")
-                if ax_idx == 0:
-                    this_ax.imshow(channel1, cmap="gray")
-                elif ax_idx == 1:
-                    this_ax.imshow(channel2, cmap="binary")
-                elif ax_idx == 2:
-                    this_ax.imshow(channel3, cmap="binary")
+        width_in = (edges[3] - edges[2]) / self.screen_dpi
+        if not channels or self.image_loader.num_channels == 1:
+            fig = matplotlib.figure.Figure(figsize=(width_in, width_in))
+            ax1 = fig.add_subplot(111)
+            ax2 = None
+            ax3 = None
+        elif channels and self.image_loader.num_channels == 2:
+            fig = matplotlib.figure.Figure(figsize=(width_in * 2, width_in))
+            ax1 = fig.add_subplot(121)
+            ax2 = fig.add_subplot(122)
+            channel2 = self.image_loader.load_frame(frame_idx, 1)
+            ax2.imshow(channel2, cmap="binary")
+            ax3 = None
+        elif channels and self.image_loader.num_channels == 3:
+            fig = matplotlib.figure.Figure(figsize=(width_in * 3, width_in))
+            ax1 = fig.add_subplot(131)
+            ax2 = fig.add_subplot(132)
+            channel2 = self.image_loader.load_frame(frame_idx, 1)
+            ax2.imshow(channel2, cmap="binary")
+            ax3 = fig.add_subplot(133)
+            channel3 = self.image_loader.load_frame(frame_idx, 2)
+            ax3.imshow(channel3, cmap="binary")
 
-            if outlines:
-                for cell in cells:
-                    coords = (np.load(cell.coords_path) +
-                              np.array([cell.offset_left, cell.offset_top]))
-                    for this_ax in [ax1, ax2, ax3]:
-                        if not this_ax:
-                            break
-                        poly = matplotlib.patches.Polygon(
-                            np.array([coords[:, 1], coords[:, 0]]).T,
-                            edgecolor="r",
-                            fill=False,
-                            lw=1,
-                        )
-                        this_ax.add_patch(poly)
+        ax1.imshow(channel1, cmap="gray")
+
+        axes = [axis for axis in [ax1, ax2, ax3] if axis]
+        for axis in axes:
+            axis.set_aspect("equal")
+            axis.axis("off")
+
+        return fig, axes
+
+    @staticmethod
+    def add_outlines(outlines, axes):
+        for outline in outlines:
+            coords = (np.load(outline.coords_path) +
+                      np.array([outline.offset_left, outline.offset_top]))
+            for axis in axes:
+                poly = matplotlib.patches.Polygon(
+                    np.array([coords[:, 1], coords[:, 0]]).T,
+                    edgecolor="r",
+                    fill=False,
+                    lw=1,
+                )
+                axis.add_patch(poly)
+
+    @staticmethod
+    def add_frame_text(frame_idx, axes):
+        txt = "F{0}".format(int(frame_idx + 1))
+        axes[0].text(
+            0.01, 0.99,
+            txt,
+            transform=axes[0].transAxes,
+            color="w",
+            fontsize=15,
+            ha="left",
+            va="top",
+        )
+
+    def add_scale_bar(self, axes, edges):
+        px_um = self.image_loader.get_pixel_conversion()
+        if not px_um:
+            px_um = 0.16
+
+        pxs = 10 / px_um
+        xleft = edges[0] + 10
+        xright = edges[0] + 15
+        ytop = edges[2] + 10
+        ybottom = edges[2] + 10 + pxs
+        scalebar = matplotlib.patches.Polygon(
+            [(ytop, xleft),
+             (ytop, xright),
+             (ybottom, xright),
+             (ybottom, xleft)],
+            facecolor="w",
+            edgecolor="w",
+            lw=2,
+        )
+        axes[0].add_patch(scalebar)
+
+    @staticmethod
+    def set_limits(axes, edges):
+        for axis in axes:
+            axis.set_xlim(edges[2:])
+            axis.set_ylim(edges[:2])
+
+    def create_movie(self, draw_outlines, frames, scale, channels):
+        out_path = os.path.join("data", "movies",
+                                self.experiment_id, self.cell_id)
+        if not os.path.exists(out_path):
+            os.makedirs(out_path)
+
+        for frame_idx, outlines in self.outlines:
+            edges = self.get_edges()
+            fig, axes = self.create_axes(frame_idx, edges, channels)
+            if draw_outlines:
+                self.add_outlines(outlines, axes)
 
             if frames:
-                txt = "F{0}".format(int(frame_idx + 1))
-                ax1.text(
-                    0.01, 0.99,
-                    txt,
-                    transform=ax1.transAxes,
-                    color="w",
-                    fontsize=15,
-                    ha="left",
-                    va="top",
-                )
+                self.add_frame_text(frame_idx, axes)
 
             if scale:
-                px_um = self.image_loader.get_pixel_conversion()
-                if not px_um:
-                    px_um = 0.16
+                self.add_scale_bar(axes, edges)
 
-                pxs = 10 / px_um
-                xleft = left_edge + 10
-                xright = left_edge + 15
-                ytop = top_edge + 10
-                ybottom = top_edge + 10 + pxs
-                scalebar = matplotlib.patches.Polygon(
-                    [(ytop, xleft),
-                     (ytop, xright),
-                     (ybottom, xright),
-                     (ybottom, xleft)],
-                    facecolor="w",
-                    edgecolor="w",
-                    lw=2,
-                )
-                ax1.add_patch(scalebar)
-
-            for this_ax in [ax1, ax2, ax3]:
-                if not this_ax:
-                    break
-                this_ax.set_xlim([bottom_edge, top_edge])
-                this_ax.set_ylim([left_edge, right_edge])
+            self.set_limits(axes, edges)
 
             fig.tight_layout()
             fig.subplots_adjust(left=0, bottom=0, right=1, top=1, wspace=0, hspace=0)
